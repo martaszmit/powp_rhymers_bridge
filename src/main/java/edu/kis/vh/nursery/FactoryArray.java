fcd2a93e0c0689e1a22916e67cfdc25dcdd05c79
Marta Szmit <marta.szmit91@gmail.com>
2018-10-17 17:48:51 +0200

    Lab2 3.1.12 -only a part, bacause I don't uderstand issue

---
+++ src/main/java/edu/kis/vh/nursery/FactoryArray.java
@@ -0,0 +1,31 @@
+package edu.kis.vh.nursery;
+
+import edu.kis.vh.nursery.factory.Rhymersfactory;
+
+public class FactoryArray implements Rhymersfactory {
+
+	@Override
+	public IIntArrayStack getStandardRhymer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IIntArrayStack getFalseRhymer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IIntArrayStack getFifoRhymer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IIntArrayStack getHanoiRhymer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
