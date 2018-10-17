5fea3553c84fe2553948e6da660eeda412329d07
Marta Szmit <marta.szmit91@gmail.com>
2018-10-17 17:02:31 +0200

    Lab2, 3.1.4

---
+++ src/main/java/edu/kis/vh/nursery/IIntArrayStack.java
@@ -0,0 +1,15 @@
+package edu.kis.vh.nursery;
+
+public interface IIntArrayStack {
+
+	void countIn(int in);
+
+	boolean callCheck();
+
+	boolean isFull();
+
+	int countOut();
+
+	int peekaboo();
+
+}
---
+++ src/main/java/edu/kis/vh/nursery/list/IIntLinkedList.java
@@ -0,0 +1,15 @@
+package edu.kis.vh.nursery.list;
+
+public interface IIntLinkedList {
+
+	void push(int i);
+
+	boolean isEmpty();
+
+	boolean isFull();
+
+	int top();
+
+	int pop();
+
+}
